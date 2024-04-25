45629c6a2bf7547bea3a9c65c1fbdb00cb27c527
mtnmc <holoholo0308@gmail.com>
2024-04-26 05:55:16 +0900

    再提出１回目

--- src/Review01.java
+++ src/Review01.java
@@ -2,15 +2,15 @@
 public class Review01 {
 
     public static void main(String[] args) {
-        double price = 1765;
-        double taxRate = 8;
-        double result;
+        int price = 1500;
+        double taxRate = 0.1;
+        int result;
         result = tax (price, taxRate);
-        System.out.println((int)price + "円の商品の税込み価格は" + (int)(price + result) +"円（消費税は" + (int)result + "円）です。");
+        System.out.println(price + "円の商品の税込み価格は" + (price + result) +"円（消費税は" + result + "円）です。");
 
     }
-    public static double tax (double price, double taxRate) {
-        double result= price/100 * taxRate;
+    public static int tax (int price, double taxRate) {
+        int result= (int)(price * taxRate);
         return result;
         }
 }
